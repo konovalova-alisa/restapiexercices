e0ecb25349a80d0dc7f6e0540e2ebc3f9956e34f
Alisa Konovalova <70326345+konovalova-alisa@users.noreply.github.com>
2021-03-07 11:47:09 +0200

    v2
    Some updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-
+document.getElementById("btn").addEventListener("click", testConnection);
+document.getElementById("btn2").addEventListener("click", postOnSpredsheet);
 
 function testConnection() {
     const url = "https://script.google.com/macros/s/AKfycbw36cJKjwdhkL26TlvqUAQ3oIla6qRY3r_e_TdgHyW6a2tqjJcqf1NlOvadA-FGn1jm/exec";
@@ -11,5 +12,21 @@ function testConnection() {
 }
 
 
+function postOnSpredsheet() {
+    const url = "https://script.google.com/macros/s/AKfycbw36cJKjwdhkL26TlvqUAQ3oIla6qRY3r_e_TdgHyW6a2tqjJcqf1NlOvadA-FGn1jm/exec";
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name: "Olivia"}) // body data type must match "Content-Type" header
+    }
+   );
+}
 
-document.getElementById("btn").addEventListener("click", testConnection);
