9590ad904b6e020345685bc0e4b1e8b7df203695
Alisa Konovalova <70326345+konovalova-alisa@users.noreply.github.com>
2021-03-07 11:48:59 +0200

    v3
    Some updates

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function postOnSpredsheet() {
         },
         redirect: 'follow', // manual, *follow, error
         // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name: "Olivia"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({name: "Test Testsson"}) // body data type must match "Content-Type" header
     }
    );
 }
